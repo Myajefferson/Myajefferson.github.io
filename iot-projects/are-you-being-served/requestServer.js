74a378990f97da043e376226ceae397d18d6577a
IoT <iot@Operations-Mac-mini-2.local>
2023-09-19 19:58:08 -0500

    saving are-you-being-served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -3,9 +3,17 @@ const http = require("http");
 const request = require("request");
 
 const port = 8686;
-function callbackFunction(error, response, body){
-
+function callbackFunction(error, response, body) {
+    if (!body || !response || (error === null && response.statusCode !== 200)){
+        res.end("bad URL\n");
+        return;
+    }
+    if(response.statusCode === 200 && "text/plain"){
+       res.end();
+       return error.toString();
+        
+    } 
 }
 http.createServer(function (req, res) {
-request('https://Myajefferson.github.io', callbackFunction);
+    request('https://Myajefferson.github.io', callbackFunction);
 }).listen(8686)
