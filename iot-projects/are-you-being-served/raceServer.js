53fa621275461e4bd7a95c8849ed58e9135b670e
IoT <iot@operationsmini2.lan>
2023-09-26 19:55:47 -0500

    saving are-you-being-served

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -3,31 +3,52 @@ const async = require("async");
 
 const port = 8686;
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
+
 
     // TODO 12: Make the whole thing parallel
-    async.series( 
+    async.parallel(
         // TODO 9: Supply an array of functions
         [
-            
+            function (callback) {
+                wrapper(callback)
+            },
+            function (callback) {
+                wrapper(callback)
+            },
+            function (callback) {
+                wrapper(callback)
+            },
+            function (callback) {
+                wrapper(callback)
+            }
         ],
+
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
-
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results)
+            for (var i = 0; i < victoryOrder.length; i++) {
+                res.write(victoryOrder[i] + "/n")
+            }
+            var endTime = d.getTime()
+            res.end(duration + "\n");
         }
     );
-    
+
 }).listen(port);
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
-function wrapper(callback){
-
+function wrapper(callback) {
+    setTimeout(callback, Math.random() * 1000)
+    callback(null, d.getTime)
 }
 
 // sortTogether takes in an array of racer names and an array of times that the racers finished the race.
@@ -35,10 +56,10 @@ function wrapper(callback){
 function sortTogether(names, times) {
     var tempList = [];
     for (var i = 0; i < names.length; i++) {
-        tempList.push({'name': names[i], 'time': times[i]});
+        tempList.push({ 'name': names[i], 'time': times[i] });
     }
 
-    tempList.sort(function(a, b) {
+    tempList.sort(function (a, b) {
         return ((a.time < b.time) ? -1 : ((a.time == b.time) ? 0 : 1));
     });
 
--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,19 +1,29 @@
 // requestServer.js file
 const http = require("http");
 const request = require("request");
-
 const port = 8686;
-function callbackFunction(error, response, body) {
-    if (!body || !response || (error === null && response.statusCode !== 200)){
-        res.end("bad URL\n");
-        return;
-    }
-    if(response.statusCode === 200 && "text/plain"){
-       res.end();
-       return error.toString();
-        
-    } 
-}
+
+
+var args = process.argv.slice(2);
 http.createServer(function (req, res) {
-    request('https://Myajefferson.github.io', callbackFunction);
+    var url = args[0] ? args[0] : "https://youtu.be/dQw4w9WgXcQ?si=wAoJVqts2Qmlko_F";
+    request('https://Myajefferson.github.io', function callbackFunction(error, response, body) {
+        console.dir({r: response.statusCode, error},{depth:null})
+        if (!body || !response || (error === null && response.statusCode !== 200)) {
+            res.end("bad URL\n");
+            return;
+        }
+        if (response.statusCode === 200 && !error === true) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(body);
+            return;
+        }
+        if (response.statusCode === 200) {
+            res.write(error);
+            return;
+        }
+        res.end("something bad happened")
+    });
 }).listen(8686)
