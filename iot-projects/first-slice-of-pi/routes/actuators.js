3fed58303113efa3f693138983c3b70271b78405
Myajefferson <jeffersonmya493@gmail.com>
2023-11-28 19:55:33 -0600

    saving first slice of Pi

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -3,5 +3,17 @@ const express = require('express'),
 	resources = require('./../resources/model');
 
 	
+router.route('/').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
+});
 
+router.route('/leds/:id').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
+});
+router.route('/leds/:1').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
+});
+router.route('/leds/:2').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
+});
 module.exports = router;
--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -1,10 +1,11 @@
 const httpServer = require('./servers/http'),
 	resources = require('./resources/model');
-
+var pirPlugin = require('./plugins/internal/pirPlugin');
 const server = httpServer.listen(resources.pi.port, function () {
 	console.log("Running the Pi on port " + resources.pi.port);
 });
 
 process.on('SIGINT', function() {
+	pirPlugin.stop();
 	process.exit();
 });
