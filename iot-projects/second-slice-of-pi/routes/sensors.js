ec0af9d841dc05ecdc44989aaaf95767441f9893
Myajefferson <jeffersonmya493@gmail.com>
2023-12-12 18:18:27 -0600

    saving first slice of Pi

--- iot-projects/second-slice-of-pi/routes/sensors.js
+++ iot-projects/second-slice-of-pi/routes/sensors.js
@@ -1,7 +1,8 @@
 const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
-
+	req.result = resources.pi.sensors;
+	next();
 
 
 module.exports = router;
--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,3 +1,7 @@
 const express = require('express'),
 	cors = require('cors');
+	require('./../middleware/converter')
+	require('body-parser').
+	app.use(bodyParser.json());
+	app.use(converter());
 module.exports = app;
