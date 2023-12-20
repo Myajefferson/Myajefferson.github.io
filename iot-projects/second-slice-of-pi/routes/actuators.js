62bb8a67d902fb0a438d429764affb207f278ce4
Myajefferson <jeffersonmya493@gmail.com>
2023-12-19 19:32:35 -0600

    saving second slice of Pi

--- iot-projects/second-slice-of-pi/routes/actuators.js
+++ iot-projects/second-slice-of-pi/routes/actuators.js
@@ -3,5 +3,22 @@ const express = require('express'),
 	resources = require('./../resources/model');
 
 	
+router.route('/').get(function (req, res, next) {
+    req.result = resources.pi.actuators.led;
+    next();
+});
 
+router.route('/leds/:id').get(function (req, res, next) {
+    req.result = resources.pi.actuators.led
+    next();
+});
+router.route('/leds/:1').get(function (req, res, next) {   
+    req.result = resources.pi.actuators.leds[1];
+    next();
+});
+router.route('/leds/:2').get(function (req, res, next) {
+    req.result = resources.pi.actuators.led[2];
+    next();
+});
 module.exports = router;
+      
--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,7 +1,23 @@
 const express = require('express'),
 	cors = require('cors');
-	require('./../middleware/converter')
-	require('body-parser').
-	app.use(bodyParser.json());
-	app.use(converter());
+	var sensorRoutes = require('./../routes/sensors');
+	var actuatorRoutes = require('./../routes/actuators');
+	var converter = require('./../middleware/converter');
+	var bodyParser = require('body-parser')
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/pi/sensors', sensorRoutes);
+app.use('/pi/actuators', sensorRoutes);
+app.get('/', function(req, res){
+   res.send('Some response for accessing the root');
+});
+
+app.get('/pi', function(req, res){
+   res.send('Some response for accessing the root');
+});
+
+app.use(converter());
 module.exports = app;
+//I have looked throught all files
+
