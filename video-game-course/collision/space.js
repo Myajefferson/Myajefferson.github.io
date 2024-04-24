23e5aa3d7661a65642b6292e1ac21fa579f569a7
Mya Jefferson <jeffersonmya493@gmail.com>
2024-04-24 00:57:57 +0000

    ghvjhbv hg hjv gh hv

--- video-game-course/collision/space.js
+++ video-game-course/collision/space.js
@@ -1,12 +1,12 @@
-(function(window, opspark, racket) {
+(function (window, opspark, racket) {
   /**
    * Creates and returns the space module. Listens for SPAWN 
    * events, adding any bodies in the event
    * @param {Object} messenger: The system wide event dispatcher.
    */
-  opspark.space = function(messenger) {
+  opspark.space = function (messenger) {
     // holds all bodies active in our space //
-    const 
+    const
       dampeningForce = 0.08,
       active = [];
 
@@ -14,7 +14,7 @@
     function onSpawn(event) {
       add(...event.bodies);
     }
-    
+
     function add(...bodies) {
       active.push(...bodies);
       return this;
@@ -34,7 +34,7 @@
         active.forEach(body => {
           // ask the body to update its velocity //
           body.update(event);
-          
+
           // update the body's position based on its new velocity //
           racket.physikz.updatePosition(body);
         });
@@ -47,31 +47,37 @@
           // compare all other bodies to bodyA, excluding bodyA: note j > -1 //
           hit: for (let j = i - 1; j > -1; j--) {
             const bodyB = active[j];
-            
+
             // TODO 1: Calculate hit test components
-            
-            
-              
+            //a^2 +b^2 = c^2
+            distanceX = bodyA.x - bodyB.x;
+            distanceY = bodyA.y - bodyB.y;
+            distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            var minimumDistance = bodyA.radius + bodyB.radius;
             // TODO 2: Do collision check: how do we know if bodies are colliding?
-            if(/* replace with collision check */ false) {
-              // console.log('hit!');
-              
+            if (minimumDistance > distance) {
+              console.log('hit!');
+
               // TODO 3: Calculate springToX and springToY 
-              
-              
-                
+              var angle = Math.atan2(distanceY, distanceX);
+
+              var springToX = Math.cos(angle) * minimumDistance + bodyA.x;
+
+              var springToY = Math.sin(angle) * minimumDistance + bodyA.y;
+
               // TODO 4: Calculate acceleration to spring-to point, factor in dampeningForce
-              
-              
-              
+              accelerationOnX = (bodyB.x - springToX) * dampeningForce;
+
+              accelerationOnY = (bodyB.y - springToY) * dampeningForce;
               // TODO 5: Apply acceleration to bodyB
-              
-              
-              
+              accelerationOnX + bodyB.velocityX;
+               accelerationOnY + bodyB.velocityY;
+
               // TODO 6: Apply inverse acceleration to bodyA
+              accerlerationOnX - bodyA.velocityX; 
+               accerlerationOnY - bodyA.velocityY;
               
-              
-              
+
             }
           }
         }
